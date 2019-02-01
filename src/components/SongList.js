c59d78b622ba56ec351d7709bef1ed5485292ddc
zarab <musfiqzarab@iut-dhaka.edu>
2019-02-01 15:36:22 +0600

    configuring connect

---
+++ src/components/SongList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class SongList extends Component {
+  state = {};
+  render() {
+    console.log(this.props);
+    return <div>SongList</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return { songs: state.songs };
+};
+
+export default connect(mapStateToProps)(SongList);
